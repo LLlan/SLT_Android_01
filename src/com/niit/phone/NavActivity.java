fdd20f917b4472d20eafc4718ecf24e7dceccd59
Administrator <Administrator@2013-20150716JG>
2015-12-03 16:54:35 +0800

    tyets

---
+++ src/com/niit/phone/NavActivity.java
@@ -0,0 +1,40 @@
+package com.niit.phone;
+
+import com.niit.slt_android_01.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+
+public class NavActivity extends Activity implements OnClickListener{
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+
+	@Override
+	public void onClick(View v) {
+		
+		
+	}
+
+	
+	
+
+
+}
